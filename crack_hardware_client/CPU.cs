46baaae40f7cc566b238ee2e789e2857f89661cf
helloyuzz <39078847+helloyuzz@users.noreply.github.com>
2020-11-12 10:07:53 +0800

    Create CPU.cs

---
+++ crack_hardware_client/CPU.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrackHardware
+{
+    [Serializable]
+    public class CPU
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+    }
+}
