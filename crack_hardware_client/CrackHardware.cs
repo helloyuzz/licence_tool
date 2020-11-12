3bd4e0721853bdc7f1282375513b75d540c5a8dd
helloyuzz <39078847+helloyuzz@users.noreply.github.com>
2020-11-12 10:07:56 +0800

    Create CrackHardware.cs

---
+++ crack_hardware_client/CrackHardware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrackHardware
+{
+    [Serializable]
+    public class CrackHardware
+    {
+        public string BOIS { get; set; }
+        public List<CPU> AllCPU { get; set; } = new List<CPU>();
+        public List<string> AllDisk { get; set; } = new List<string>();
+        public List<string> AllNetCard { get; set; } = new List<string>();
+        public string FirstCPUName {
+            get {
+                if (AllCPU.Count > 0) {
+                    return AllCPU[0].Name;
+                }
+                return "未检测到CPU";
+            }
+        }
+
+        internal string GetFirstCPU()
+        {
+            string cpu = "";
+            if (AllCPU.Count > 0) {
+                cpu = AllCPU[0].Manufacturer.ToUpper() + AllCPU[0].Id.ToUpper();
+            }
+
+            return cpu;
+        }
+    }
+}
